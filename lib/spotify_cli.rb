546217cfeaf940eea127cb5d1e590629feb0713f
MattyCodes <matt.nick.fagioli@gmail.com>
2020-01-28 17:35:07 -0500

    Setup for the initial auth and configuration of the gem as well as the executable.

--- lib/spotify_cli.rb
+++ lib/spotify_cli.rb
@@ -1,5 +1,11 @@
 require "spotify_cli/version"
 
 module SpotifyCli
+  # Generic error class definition.
   class Error < StandardError; end
+
+  # Require the necessary classes/modules.
+  require 'spotify_cli/setup'
+  require 'spotify_cli/authentication'
+  require 'spotify_cli/initialization'
 end
---
+++ lib/spotify_cli/initialization.rb
@@ -0,0 +1,7 @@
+module SpotifyCli
+  module Initialization
+    def initialize_cli!
+      puts 'Initializing...'
+    end
+  end
+end
